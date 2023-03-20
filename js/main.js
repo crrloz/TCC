475d776d2cd0b82099a38981e39badeb6d75740f
Carlos <carloseduardobispodonascimento@gmail.com>
2023-03-19 23:02:27 -0300

    hmm

---
+++ js/main.js
@@ -0,0 +1,92 @@
+
+(function ($) {
+    "use strict";
+
+    /*[DE VOLTA AO TOPO]
+    ===========================================================*/
+    var windowH = $(window).height()/2;
+
+    $(window).on('scroll',function(){
+        if ($(this).scrollTop() > windowH) {
+            $("#myBtn").css('display','flex');
+        } else {
+            $("#myBtn").css('display','none');
+        }
+    });
+
+    $('#myBtn').on("click", function(){
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+    /*[MOSTRAR/ESCONDER SIDEBAR]
+    ===========================================================*/
+    $('body').append('<div class="overlay-sidebar trans-0-4"></div>');
+    var ovlSideBar = $('.overlay-sidebar');
+    var btnShowSidebar = $('.btn-show-sidebar');
+    var btnHideSidebar = $('.btn-hide-sidebar');
+    var sidebar = $('.sidebar');
+
+    $(btnShowSidebar).on('click', function(){
+        $(sidebar).addClass('show-sidebar');
+        $(ovlSideBar).addClass('show-overlay-sidebar');
+    })
+
+    $(btnHideSidebar).on('click', function(){
+        $(sidebar).removeClass('show-sidebar');
+        $(ovlSideBar).removeClass('show-overlay-sidebar');
+    })
+
+    $(ovlSideBar).on('click', function(){
+        $(sidebar).removeClass('show-sidebar');
+        $(ovlSideBar).removeClass('show-overlay-sidebar');
+    })
+
+    /*[PAUSAR/RODAR VÍDEO]
+	===========================================================*/
+
+    var my_video = document.querySelector(".my_video");
+    var isPlaying = false;
+    
+    $(my_video).on("click", function () {
+        if (isPlaying) {
+            my_video.pause()
+        } else {
+            my_video.play();
+        }
+    });
+
+    $(my_video).on("playing", function() {
+        isPlaying = true;
+    });
+
+    $(my_video).on("pause", function() {
+        isPlaying = false;
+    });
+
+    var my_audio = $('.my_audio');
+
+    $(my_audio).on("click", function(e){
+        my_video.muted = !my_video.muted;
+    });
+
+    /*[HEADER FIXA]
+    ===========================================================*/
+    var header = $('header');
+    var logo = $(header).find('.logo img');
+    var linkLogo1 = $(logo).attr('src');
+    var linkLogo2 = $(logo).data('logofixed');
+
+
+    $(window).on('scroll',function(){
+        if($(this).scrollTop() > 5 && $(this).width() > 992) {
+            $(logo).attr('src',linkLogo2);
+            $(header).addClass('header-fixed');
+        }
+        else {
+            $(header).removeClass('header-fixed');
+            $(logo).attr('src',linkLogo1);
+        }
+        
+    });
+
+})(jQuery);
